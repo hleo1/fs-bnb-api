39bedc0f80a3b0f3e00e44f0dcfc468682062c51
Hubert <leo.hubert3@gmail.com>
2019-08-07 15:31:08 +0200

    new

--- src/api/booking-routes.js
+++ src/api/booking-routes.js
@@ -4,6 +4,7 @@ const Booking = require("../models/bookings-model");
 
 //CREATE, READ, UPDATE, DELETE USERS
 router.get("/", (req, res) => {
+    
     res.send("Hey!");
     // Booking.prototype.getBookings().then(bookings => {
     //     res.send(bookings);
