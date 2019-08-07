4ade5c407205dfc3fa0961d2fb3229a971fd4a64
Hubert <leo.hubert3@gmail.com>
2019-08-07 15:38:34 +0200

    new

--- src/api/user-routes.js
+++ src/api/user-routes.js
@@ -10,7 +10,7 @@ router.get("/", (req, res) => {
         res.send(err);
     }); 
 });
-
+//
 //get user by ID
 router.get("/:id", (req, res) => {
     User.prototype.getUserID(req.params.id).then(users => {
